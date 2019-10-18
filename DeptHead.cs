b58d36646059dbc81254a4f424efd434450beccb
Antoine Williams <54339074+antoineel9@users.noreply.github.com>
2019-10-18 15:41:13 -0500

    Add files via upload

---
+++ DeptHead.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FourthGear.Data
+{
+    public class DeptHead
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int DHId { get; set; }
+        [ForeignKey("DHId")]
+
+        public  string FName{ get; set; }
+        public string LName{ get; set; }
+        public string Location { get; set; }
+        public string City { get; set; }
+    }
+}
